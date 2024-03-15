f8ca174656f058acdb8b91e06a1f0b4f0dc03ba3
partkyle <partkyle@gmail.com>
2024-03-15 00:02:16 -0700

    fade out the damage numbers

--- damage_notifier.gd
+++ damage_notifier.gd
@@ -6,7 +6,7 @@ func popup_damage(event: DamageEvent):
 	var d := damage_popup.instantiate()
 	d.text = '%d' % event.damage
 	add_child(d)
-	d.transform.origin = event.global_position
+	d.global_position = event.global_position
 
 func _on_signal_bus_damage_dealt(event: DamageEvent):
 	popup_damage(event)
--- damage_popup.gd
+++ damage_popup.gd
@@ -8,11 +8,15 @@ extends Label3D
 
 var acc := 0.0
 
-@onready var double_scale := scale * randf_range(1.8, 2.2)
+@onready var double_scale := scale * randf_range(1.2, 2.0)
 
 func _process(delta):
 	transform.origin += direction*speed*delta
 	scale = scale.lerp(double_scale, delta)
+
+	modulate.a = lerp(modulate.a, 0.0, delta * 10.0)
+	outline_modulate.a = lerp(outline_modulate.a, 0.0, delta * 10.0)
+
 	acc += delta
 	if acc > time:
 		queue_free()
