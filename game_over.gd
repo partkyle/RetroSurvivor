f5076332d9fbbf507cb6337b8901a2f31cf96200
partkyle <partkyle@gmail.com>
2024-03-24 20:47:23 -0700

    game reload on fail
    - SceneManager singleton set up in project settings.

--- game_over.gd
+++ game_over.gd
@@ -1,6 +1,13 @@
 extends CanvasItem
 
+var dead := false
 
 func _on_player_death():
+	dead = true
 	show()
 	get_tree().paused = true
+
+func _input(event: InputEvent):
+	if dead and event.is_action("game_reload"):
+		get_tree().paused = false
+		SceneManager.load_arena()
