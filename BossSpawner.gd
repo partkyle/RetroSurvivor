643e994efaab85068d379a73823e08007ebeee8c
partkyle <partkyle@gmail.com>
2024-04-01 15:51:26 -0700

    adjust some spawning logic to make the boss and the extraction spawn immediately.

--- BossSpawner.gd
+++ BossSpawner.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var boss : PackedScene
 @export var extraction : PackedScene
+@onready var extraction_parent : Node = $".."
 @export var signal_bus : SignalBus
 @export var spawn_near_target : Node3D
 @export var spawn_parent : Node3D
@@ -20,14 +21,14 @@ func spawn_boss():
 	e.global_position = spawn_near_target.global_position + Vector3(randf_range(-1.0, 1.0), 0.0, randf_range(-1.0, 1.0)) * 10.0
 	e.target = player
 	e.set_signal_bus(signal_bus)
+	e.connect('death', spawn_extraction)
 	spawn_parent.add_child(e)
 	signal_bus.enemy_spawned.emit(e)
-	e.connect('tree_exited', spawn_extraction)
 	boss_spawned = true
 
-func spawn_extraction():
+func spawn_extraction(_enemy: Enemy):
 	var e := extraction.instantiate()
-	get_tree().root.add_child(e)
+	extraction_parent.add_child(e)
 	e.global_position = Vector3(0.0, 0.01, 0.0)
 
 
@@ -35,7 +36,7 @@ func _ready():
 	total = watch_list.size()
 	print('waiting for %d totems' % total)
 	for w in watch_list:
-		w.connect('tree_exited', func(): total -= 1)
+		w.connect('tree_exiting', func(): total -= 1)
 
 func _process(delta: float):
 	if total <= 0 && not boss_spawned:
