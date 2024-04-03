9368002b81d52f7ed1aaaf39eaf14fe7b4a1a733
partkyle <partkyle@gmail.com>
2024-04-03 13:58:20 -0700

    add a level up debug

--- levels/debug.gd
+++ levels/debug.gd
@@ -8,3 +8,5 @@ func _input(event: InputEvent):
 	if event is InputEventKey and event.keycode == KEY_F2:
 		for i in range(1000):
 			xpSpawner.spawn_xp_at_position(player.global_position)
+	if event is InputEventKey and event.keycode == KEY_F3:
+		LevelStats.current_stats.level += 1
